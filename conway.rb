cb68f76466b189e4819d4be21d7dac6b564e50b8
fernando amezcua alcantar <fernandoamezcuaalcantar@MacBook-Pro-de-fernando.local>
2018-09-20 13:49:30 -0500

    iniciando el juego de la vida

---
+++ conway.rb
@@ -0,0 +1,124 @@
+class Conway 
+    def muestras
+        pobladores = 15
+        celulas = [pobladores]
+        for i in 0...pobladores
+            celulas[i] = [pobladores]
+            for j in 0...pobladores
+                celulas[i][j] = rand(0..1)
+            end
+        end
+        return celulas
+    end    
+
+
+    def buscarVecinos(x,y)
+
+        contador = 0;
+
+        #Revisar celda izquierda de la posición actual
+        if estado(x-1, y) 
+            contador += 1
+        end
+
+        # Revisar superior izquierda
+        if estado(x-1, y-1)
+            contador += 1
+        end
+
+        # Revisar inferior izquierda
+        if estado(x-1, y+1)
+            contador += 1
+        end
+
+        # Revisar celda de arriba del posicion actual
+        if estado(x  , y-1)
+            contador += 1
+        end
+
+        # celda derecha de la posicion actual
+        if estado(x  , y+1)
+            contador += 1
+        end
+
+        #  Esquina superior izquierda de la posicion actual
+        if estado(x+1, y-1)
+            contador += 1
+        end
+
+        # Revisar esquina inferior derecha
+        if estado(x+1, y+1)
+            contador += 1
+        end 
+
+        # Revisar celda de abajo de la posicion actual
+        if estado(x+1, y)
+            contador += 1
+        end
+        
+        return contador;
+    end
+    
+    def estado(x,y) 
+        if muestras[x] 
+            if muestras[x][y] == 1
+                return true;
+            else
+                return false;
+            end
+        end
+    end
+
+    def generaciones     
+        for x in 0...muestras.size
+            for y in 0...muestras.size
+
+                valor    = muestras[x][y]
+                contador = buscarVecinos(x,y)
+                result   = 0
+
+
+                # Si esta viva y tiene menos de dos vecinos muere
+                if valor && contador < 2
+                    result = 0;
+                end
+
+                # si tiene valor y tiene 2 o tres vecinos vive 
+                if valor && (contador == 2 || contador == 3)
+                    result = 1;
+                end
+
+                # si tiene mas de tres vecinos muere
+                if valor && contador > 3
+                    result = 0;
+                end
+                
+                # Si no tiene valor y tiene tres vecinos vivos Nace
+                if !valor && contador == 3
+                    result = 1;
+                end
+
+                muestras[x][y] = result;
+
+            end
+
+            return muestras
+        end
+    end
+end
+
+def play
+
+    puts "Iniciar generación ¿y/n?"
+    response = gets.chomp
+
+    if response == "y"
+        nextGeneration = true
+        print Conway.new.generaciones
+        play
+    elsif response == "n"
+        nextGeneration = false
+    end
+end
+
+play
